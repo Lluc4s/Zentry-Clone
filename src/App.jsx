6a705d76d845d8a7b886130b5859ea027050cbca
Lucas J. <phuttiphongbank@gmail.com>
2025-05-02 00:38:08 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 px-4 py-2 text-3xl font-bold text-yellow-100 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return (<div className="relative h-dvh w-screen overflow-x-hidden">Hero</div>);
+};
+
+export default Hero;
